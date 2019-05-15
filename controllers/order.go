900b8b03dd61bbb09d1b3ac3021f7f114886822c
HarlanC <wawacry@qq.com>
2019-05-15 21:57:33 +0800

    implement order.go

--- controllers/order.go
+++ controllers/order.go
@@ -1,1 +1,27 @@
 package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"github.com/moshopserver/models"
+	"github.com/moshopserver/utils"
+)
+
+type OrderController struct {
+	beego.Controller
+}
+
+func (this *OrderController) Order_List() {
+
+	o := orm.NewOrm()
+	ordertable := new(models.NideshopOrder)
+	var orders []models.NideshopOrder
+	o.QueryTable(ordertable).Filter("user_id", getLoginUserId()).All(&orders)
+
+	firstpagedorders := utils.GetPageDataInterface(orders, 1, 10)
+	var newpageorders []models.NideshopOrder
+
+	for _, val := range firstpagedorders.Data.([]models.NideshopOrder) {
+
+	}
+}
